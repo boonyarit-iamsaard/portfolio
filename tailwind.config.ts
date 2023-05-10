222aa3d64bdad829b650272f781ce4e8121c0856
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-05-10 15:50:49 +0700

    build: clean up and add initial tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,45 @@
 import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '100%',
+        md: '900px',
+        lg: '900px',
+        xl: '900px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+      },
+      screens: {
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+      },
+      supports: {
+        'backdrop-blur-md': 'backdrop-filter: blur(12px)',
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+          '60%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+      animation: {
+        wave: 'wave 2.5s ease-in-out infinite',
       },
     },
   },
