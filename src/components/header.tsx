38c7929924c6f5110e31145f5508f6165df2e39f
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-05-18 19:52:38 +0700

    build: add navigation menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,52 @@
 import Link from 'next/link';
 import { IconAppWindow, IconMenu2 } from '@tabler/icons-react';
 
+import { Button } from './ui/button';
+
+const links = [
+  {
+    name: 'Projects',
+    href: '/projects',
+  },
+  {
+    name: 'Articles',
+    href: '/articles',
+  },
+  {
+    name: 'About',
+    href: '/about',
+  },
+];
+
 export function Header() {
   return (
     <header className="fixed z-10 flex h-16 w-full items-center border-b bg-background shadow-sm supports-backdrop-blur-md:bg-background/75 supports-backdrop-blur-md:backdrop-blur-md">
       <div className="container flex items-center justify-between">
-        <Link
-          href="/"
-          className="block rounded p-2 outline-none ring-secondary transition-all duration-300 ease-in-out hover:bg-secondary focus:bg-secondary focus:ring-2 focus:ring-offset-2"
-        >
-          <span className="sr-only">Home</span>
-          <IconAppWindow />
-        </Link>
-        <button
-          type="button"
-          className="rounded p-2 outline-none ring-secondary transition-all duration-300 ease-in-out hover:bg-secondary focus:bg-secondary focus:ring-2 focus:ring-offset-2"
-        >
-          <span className="sr-only">Menu</span>
-          <IconMenu2 />
-        </button>
+        <Button asChild size="icon" variant="ghost">
+          <Link href="/">
+            <span className="sr-only">Home</span>
+            <IconAppWindow />
+          </Link>
+        </Button>
+
+        <nav className="hidden md:block">
+          <ul className="flex items-center space-x-4">
+            {links.map(link => (
+              <li key={link.name}>
+                <Button asChild variant="ghost">
+                  <Link href={link.href}>{link.name}</Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <Button asChild size="icon" variant="ghost" className="md:hidden">
+          <span>
+            <span className="sr-only">Home</span>
+            <IconMenu2 />
+          </span>
+        </Button>
       </div>
     </header>
   );
