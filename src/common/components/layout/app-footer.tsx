3305dcdd4ad53c077da72fc6764a49d08ddf20b6
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-04-06 21:20:26 +0700

    style: change app footer background color

--- src/common/components/layout/app-footer.tsx
+++ src/common/components/layout/app-footer.tsx
@@ -2,7 +2,7 @@ import { SocialLinks } from '@/common/components/social-links';
 
 export function AppFooter() {
   return (
-    <footer className="py-8">
+    <footer className="bg-muted/50 py-4">
       <div className="container flex flex-col items-center justify-between gap-4 md:flex-row md:gap-0">
         <p className="text-muted-foreground text-sm">
           &copy; {new Date().getFullYear()} Boonyarit Iamsa-ard. All rights
