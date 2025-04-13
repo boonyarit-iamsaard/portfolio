17968244105923f1c45e2580ff49d14fa1dcc30e
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-04-13 12:38:35 +0700

    build: remove potential unused environment variables

--- src/core/configs/env.config.ts
+++ src/core/configs/env.config.ts
@@ -3,9 +3,6 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    APP_NAME: z.string().min(1),
-    APP_TIMEZONE: z.string().min(1),
-    APP_URL: z.string().url(),
     NODE_ENV: z.string().min(1),
     MAIL_HOST: z.string().min(1),
     MAIL_PORT: z.coerce.number(),
@@ -15,14 +12,9 @@ export const env = createEnv({
     MAIL_FROM_ADDRESS: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_APP_NAME: z.string().min(1),
-    NEXT_PUBLIC_APP_TIMEZONE: z.string().min(1),
-    NEXT_PUBLIC_APP_URL: z.string().url(),
+    //
   },
   runtimeEnv: {
-    APP_NAME: process.env.APP_NAME,
-    APP_TIMEZONE: process.env.APP_TIMEZONE,
-    APP_URL: process.env.APP_URL,
     NODE_ENV: process.env.NODE_ENV,
     MAIL_HOST: process.env.MAIL_HOST,
     MAIL_PORT: process.env.MAIL_PORT,
@@ -30,8 +22,5 @@ export const env = createEnv({
     MAIL_PASSWORD: process.env.MAIL_PASSWORD,
     MAIL_FROM_NAME: process.env.MAIL_FROM_NAME,
     MAIL_FROM_ADDRESS: process.env.MAIL_FROM_ADDRESS,
-    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-    NEXT_PUBLIC_APP_TIMEZONE: process.env.NEXT_PUBLIC_APP_TIMEZONE,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 });
